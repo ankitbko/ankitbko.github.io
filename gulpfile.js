50b095e65fb1d68b812fe4a944e9eb92083fbf08
Ankit <ankit.sinha@mindtree.com>
2016-03-28 22:37:01 +0530

    Minified css

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const uglifycss = require('gulp-uglifycss')
+
+const cssFiles = '_css/**/*.css';
+
+gulp.task('css', function() {
+  return gulp.src(cssFiles)
+    .pipe(concat('all.min.css'))
+	.pipe(uglifycss({
+      "maxLineLen": 80,
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('public/css'))
+});
+
+gulp.task('default', ['css']);
